1d7464eb3b156c72f64c63c8c2ebf08ccac45cf4
ahmetcankaratas <karatascanahmet@yahoo.com>
2023-01-11 16:57:17 +0300

    add hero section with particles

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './page/home/home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HomeRoutingModule } from './home.routing';
+import { HeroComponent } from './page/home/hero/hero.component';
+import { NgParticlesModule } from 'ng-particles';
+import { ParticleComponent } from './page/home/particle/particle.component';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule, HomeRoutingModule],
-  exports: [HomeComponent],
+  declarations: [HomeComponent, HeroComponent, ParticleComponent],
+  imports: [CommonModule, SharedModule, HomeRoutingModule, NgParticlesModule],
+  exports: [HomeComponent, HeroComponent, ParticleComponent],
 })
 export class HomeModule {}
