5c898955327de6d4165684a4cf4b1f8ba65cddce
ahmetcankaratas <karatascanahmet@yahoo.com>
2023-01-13 18:03:18 +0300

    add login page

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  inValidSubmit = false;
+  loginData = {
+    email: '',
+    password: ''
+  };
 
+  constructor() { }
+  ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm){
+    if (!form.valid){
+      console.log('Invalid form')
+      this.inValidSubmit = true;
+      return;
+    }
+
+    this.inValidSubmit = false;
+    this.loginData.email = form.value.email;
+    this.loginData.password = form.value.password;
+    console.log(form.value);
+    form.reset();
+  }
 }
