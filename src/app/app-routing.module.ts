80ec8f6b2235ee9ef6801ec517a7eddb81bac0c6
ahmetcankaratas <karatascanahmet@yahoo.com>
2023-01-16 15:29:59 +0300

    add auth guard to scanner

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './modules/auth/auth.guard';
 
 const routes: Routes = [
   {
@@ -10,6 +11,7 @@ const routes: Routes = [
   },
   {
     path: 'scanner',
+    canActivate: [AuthGuard],
     loadChildren: () =>
       import('./modules/scanner/scanner.module').then((m) => m.ScannerModule),
   },
