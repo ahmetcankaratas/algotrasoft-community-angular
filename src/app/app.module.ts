82110061796de09abb6c18d3b923120fe8a1cfce
ahmetcankaratas <karatascanahmet@yahoo.com>
2023-01-19 13:41:37 +0300

    add token interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { TokenInterceptor } from './core/interceptor/token.interceptor';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, SharedModule, AppRoutingModule],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/data/service/data-storage.service.ts
+++ src/app/data/service/data-storage.service.ts
@@ -1,18 +1,18 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { exhaustMap, take } from 'rxjs';
+import { AuthService } from 'src/app/core/service/auth.service';
 import { BacktestResponseResult } from '../types/backtest';
 import { MarketResponse } from '../types/market';
-import { ResultService } from './result.service';
-
 @Injectable({
   providedIn: 'root',
 })
 export class DataStorageService {
-  constructor(private resultService: ResultService, private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
   getStocks() {
     return this.http.get<MarketResponse>(
-      'https://algotrasoft-community-angular-default-rtdb.europe-west1.firebasedatabase.app/market.json'
+      `https://algotrasoft-community-angular-default-rtdb.europe-west1.firebasedatabase.app/market.json`
     );
   }
 
